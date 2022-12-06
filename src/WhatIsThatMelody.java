bfd8753f475cd7c0e57243317307dcddaf900e20
FlyBiggy <chtijuju6@gmail.com>
2022-12-06 09:23:56 +0100

    fonction init et variables globales

--- src/WhatIsThatMelody.java
+++ src/WhatIsThatMelody.java
@@ -1,12 +1,35 @@
 class WhatIsThatMelody extends Program {
 
+    public String nom = "";
+    public String difficulte = "";
+
     ////////////////////////////////          TESTS            ////////////////////////////////
 
     
 
     ////////////////////////////////        FONCTIONS          ////////////////////////////////
 
-
+    void init() {
+        boolean bon = false;
+        print("Quel est votre nom ? ");
+        nom = ReadString();
+        print("Quelle difficulté  voulez-vous ? A) facile, B) Moyen, C) Difficile");
+        char choix = readChar().toUpperCase();
+        while (!bon) {
+            if (choix == 'A'){
+                difficulte = "facile";
+                bon = true;
+            } else if (choix == 'B') {
+                difficulte = "moyen";
+                bon = true;
+            } else if (choix == 'C') {
+                difficulte = "difficile";
+                bon = true;
+            } else {
+                print("choisissez une lettre valide entre A, B ou C");
+            }
+        }
+    }
 
     ////////////////////////////////    PROGRAMME PRINCIPAL    ////////////////////////////////
 
