e0b233150c5a4a5aa84ca7d2bbc231b9b8a3101e
Paul <paul.degraeve.etu@univ-lille.fr>
2022-12-07 14:37:33 +0100

    base faite

--- src/WhatIsThatMelody.java
+++ src/WhatIsThatMelody.java
@@ -4,6 +4,8 @@ class WhatIsThatMelody extends Program {
 
     public String nom = "";
     public String difficulte = "";
+    public String type = "";
+    public String titreChoisi = "";
     
 
     ////////////////////////////////          TESTS            ////////////////////////////////
@@ -12,32 +14,66 @@ class WhatIsThatMelody extends Program {
 
     ////////////////////////////////        FONCTIONS          ////////////////////////////////
 
-    void init() {
+    void start() {
         boolean bon = false;
+        char choix;
         print("Quel est votre nom ? ");
         nom = readString();
-        print("Quelle difficulté  voulez-vous ? A) facile, B) Moyen, C) Difficile");
-        char choix = readChar()
-        ;
+        print("Quelle difficulté voulez-vous ? A) Facile, B) Moyen, C) Difficile\n>> ");
         while (!bon) {
+            choix = readChar();
             if (choix == 'A' || choix == 'a'){
                 difficulte = "facile";
                 bon = true;
             } else if (choix == 'B' || choix == 'b') {
                 difficulte = "moyen";
                 bon = true;
-            } else if (choix == 'C' || chois == 'c') {
+            } else if (choix == 'C' || choix == 'c') {
                 difficulte = "difficile";
                 bon = true;
             } else {
-                print("choisissez une lettre valide entre A, B ou C");
+                print("Choisissez une lettre valide entre A, B ou C !\n>> ");
             }
+        }
+        bon = false;
+        print("Quel mode voulez-vous ? A) Conjugaison, B) Orthographe, C) Mémoire\n>> ");
+        while (!bon) {
+            choix = readChar();
+            if (choix == 'A' || choix == 'a'){
+                type = "CONJUGAISON";
+                bon = true;
+            } else if (choix == 'B' || choix == 'b') {
+                type = "ORTHOGRAPHE";
+                bon = true;
+            } else if (choix == 'C' || choix == 'c') {
+                type = "MEMOIRE";
+                bon = true;
+            } else {
+                print("Choisissez une lettre valide entre A, B ou C !\n>> ");
+            }
+        }
+        println();
+        println("Vous avez choisi la difficulté " + difficulte + " avec le mode " + type);
+        println();
+    }
+
+    void afficher(String[] titres) {
+        for (int i = 0; i<length(titres); i++) {
+            println(i+1 + ") " + titres[i]);
         }
     }
 
-    /*Chanson choixChanson() {
-        
-    }*/
+    void choixChanson(CSVFile csv) {
+        String[] titres = new String[rowCount(csv)];
+        for (int line = 0; line<rowCount(csv); line++) {
+            titres[line] = getCell(csv, line, 0);
+        }
+        println("Quelles comptines voulez-vous ?");
+        afficher(titres);
+        int choix = readInt() - 1;
+        titreChoisi = titres[choix];
+        println("Vous avez choisi '" + titreChoisi + "'");
+    }
 
     void print(CSVFile csv) {
         for(int line = 0; line<rowCount(csv); line++) {
@@ -48,10 +84,12 @@ class WhatIsThatMelody extends Program {
         }
     }
 
-    void algorithm() {
-        print(loadCSV("../ressources/chanson.csv",','));
-        print(loadCSV("../ressources/parole.csv",','));
-    }
     ////////////////////////////////    PROGRAMME PRINCIPAL    ////////////////////////////////
+
+    void algorithm() {
+        start();
+        String chansonsCSV = "../ressources/chanson.csv";
+        choixChanson(loadCSV(chansonsCSV));
+    }
 
 }
