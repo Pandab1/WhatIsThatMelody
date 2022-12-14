e23c77b42347015117545035421b5ab8b2b3979b
Paul <paul.degraeve.etu@univ-lille.fr>
2022-12-14 15:23:39 +0100

    trop de trucs

--- src/WhatIsThatMelody.java
+++ src/WhatIsThatMelody.java
@@ -4,6 +4,8 @@ class WhatIsThatMelody extends Program {
 
     ////////////////////////////        VARIABLES GLOBALES          ///////////////////////////
 
+    public final String ANSI_GREEN = "\u001B[32m";
+    public final String ANSI_WHITE = "\u001B[37m";
     public String nom = "";
     public String difficulte = "";
     public String type = "";
@@ -14,11 +16,34 @@ class WhatIsThatMelody extends Program {
     
     ////////////////////////////////        FONCTIONS          ////////////////////////////////
 
-    void start() {
+    void joueur(String[][] content) {
+        int choix;
+        boolean bon = false;
+        print("As-tu déjà joué ? (1 pour oui ou 2 pour non)\n>> ");
+        while (!bon) {
+            choix = readInt();
+            if (choix == 1) {
+                print("Heureux de te revoir ! Quel était ton nom ?\n>> ");
+                nom = readString().toLowerCase();
+                for (int i = 0; i < length(content); i++) {
+                    if (equals(content[i][0], nom)) {
+                        score = Integer.parseInt(content[i][1]);
+                    }
+                }
+                bon = true;
+            } else if (choix == 2) {
+                print("Bienvenue parmis nous ! Quel est ton nom ?\n>> ");
+                nom = readString().toLowerCase();
+                bon = true;
+            } else {
+                print("Choisissez un chiffre valide entre 1 ou 2 !\n>> ");
+            }
+        }
+    }
+
+    void choixDifficulte() {
         boolean bon = false;
         char choix;
-        print("Quel est votre nom ? ");
-        nom = readString();
         print("Quelle difficulté voulez-vous ? A) Facile, B) Moyen, C) Difficile\n>> ");
         while (!bon) {
             choix = readChar();
@@ -35,7 +60,11 @@ class WhatIsThatMelody extends Program {
                 print("Choisissez une lettre valide entre A, B ou C !\n>> ");
             }
         }
-        bon = false;
+    }
+
+    void choixMode() {
+        boolean bon = false;
+        char choix;
         print("Quel mode voulez-vous ? A) Conjugaison, B) Orthographe, C) Mémoire\n>> ");
         while (!bon) {
             choix = readChar();
@@ -52,9 +81,6 @@ class WhatIsThatMelody extends Program {
                 print("Choisissez une lettre valide entre A, B ou C !\n>> ");
             }
         }
-        println();
-        println("Vous avez choisi la difficulté " + difficulte + " avec le mode " + type);
-        println();
     }
 
     void afficher(String[] titres) {
@@ -71,12 +97,13 @@ class WhatIsThatMelody extends Program {
         }
         println("Quelles comptines voulez-vous ?");
         afficher(titres);
+        print(">> ");
         int choix = readInt() - 1;
         while (!bon) {
             if (choix >= 0 && choix < length(titres)) {
                 bon = true;
             } else {
-                print("Veuillez prendre un chiffre disponible !\n>>");
+                print("Veuillez prendre un chiffre disponible !\n>> ");
                 choix = readInt() - 1;
             }
         }
@@ -86,10 +113,10 @@ class WhatIsThatMelody extends Program {
     }
 
     String init(CSVFile chansonCSV, CSVFile paroleCSV) {
-        String retour = "La comptine " + chanson.titre + " est une comptine écrite par ";
+        String retour = "La comptine " + ANSI_GREEN + chanson.titre + ANSI_WHITE + " est une comptine écrite par ";
         chanson.auteur = getCell(chansonCSV, ligneTitre, 1);
         chanson.dateSortie = getCell(chansonCSV, ligneTitre, 2);
-        retour += chanson.auteur + " en/au " + chanson.dateSortie + "\n\n";
+        retour += ANSI_GREEN + chanson.auteur + ANSI_WHITE + " en/au " + chanson.dateSortie + "\n\n";
         paroles.preced = "";
         paroles.reponse = "";
         paroles.type = "";
@@ -124,37 +151,68 @@ class WhatIsThatMelody extends Program {
 
     void print(CSVFile csv) {
         for(int line = 0; line<rowCount(csv); line++) {
-            for (int column = 0; column<columnCount(csv, line); column++){
-                print(getCell(csv, line, column )+" | ");
+            for (int column = 0; column<columnCount(csv, line); column++) {
+                print(getCell(csv, line, column )+" ");
             }
             println();
         }
     }
 
+    String[][] load(CSVFile csv) {
+        String[][] save = new String[25][2];
+        for (int line = 0; line<rowCount(csv); line++) {
+            for (int column = 0; column<columnCount(csv, line); column++) {
+                save[line][column] = getCell(csv, line, column);
+            }
+        }
+        return save;
+    }
+
+    void save(String[][] content, String fileName) {
+        content[0][0] = nom;
+        content[0][1] = score + "";
+        saveCSV(content, fileName);
+    }
+
     ////////////////////////////////    PROGRAMME PRINCIPAL    ////////////////////////////////
 
     void algorithm() {
-        start();
+
         boolean fin = false;
-        char fini = 'a';
+        char fini;
+        String save = "../ressources/save_file.csv";
         String chansonsCSV = "../ressources/chanson.csv";
-        String paroleCSV = "../ressources/parole.csv";
-        String presentateurCSV = "../ressources/Presentateur.csv";
+        String paroleCSV = "";
+        String presentateurCSV = "../ressources/presentateur.csv";
+        String texte = "";
+
+        String[][] sauvegarde = load(loadCSV(save));
+
+        joueur(sauvegarde);
+        choixDifficulte();
+
         while(!fin) {
+            choixMode();
+            paroleCSV = "../ressources/parole_" + difficulte + ".csv";
+            println();
+            println("Vous avez choisi la difficulté " + difficulte + " avec le mode " + type);
+            println();
             choixChanson(loadCSV(chansonsCSV));
             println();
-            String texte = init(loadCSV(chansonsCSV), loadCSV(paroleCSV));
+            texte = init(loadCSV(chansonsCSV), loadCSV(paroleCSV));
             print(loadCSV(presentateurCSV));
             print(texte);
             reponse();
             println();
-            println("Ton score est de " + score);
-            print("Veux-tu continuer ?? A) Oui B) Non\n>>");
+            println("Ton score est de : " + score);
+            println();
+            print("Veux-tu continuer ?? A) Oui B) Non\n>> ");
             fini = readChar();
             if (fini == 'b' || fini == 'B') {
                 fin = true;
             }
         }
+        save(sauvegarde, save);
         println("Bien joué tu as atteint le score de " + score + " points !!");
     }
 }
